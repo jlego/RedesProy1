beba4fa7d636eecca50fc6da47b4860df00d931b
Jose Lezama Gonzalez <joseglego@gmail.com>
2013-01-24 19:33:05 -0430

    Primer Commit

---
+++ funciones.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int verificacion(int obj, int ctdd, char **argv) {
+  int val = 0;
+  int nom = 0;
+  int cap = 0;
+  int inv = 0;
+  int con = 0;
+  int fic = 0;
+  char tmp1[256] = "";
+  char tmp2[256] = "";
+  if (ctdd <> obj) {
+    val = 1;
+  }
+  for (int i =0;i < ctdd; i+=1){
+    
+  }
+
+}
